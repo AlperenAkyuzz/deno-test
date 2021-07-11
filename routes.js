b2e158673cde1e84876a0d0a66937e4771379ed4
Alperen <alperenakyuz@yandex.com>
2021-07-11 19:46:43 +0300

    Third Commit

---
+++ routes.js
@@ -0,0 +1,14 @@
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Merhaba Dünya!";
+  })
+  .get("/messages", (context) => {
+    context.response.body = messages;
+  })
+  .post("/messages", async (context) => {
+    const message = await context.request.body().value;
+    messages.push(message);
+    channel.postMessage(message);
+    context.response.body = messages;
+  });
