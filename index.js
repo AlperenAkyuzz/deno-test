7a095cbe3b355f81e3f0a411dad5734ac89c9e4d
Alperen <alperenakyuz@yandex.com>
2021-07-11 19:54:53 +0300

    Testtt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
-import { routes } from 'https://github.com/AlperenAkyuzz/deno-test/blob/master/routes.js';
+import { router } from 'https://github.com/AlperenAkyuzz/deno-test/blob/master/routes.js';
 
 const messages = [];
 const channel = new BroadcastChannel("chat");
@@ -8,24 +8,10 @@ channel.onmessage = (event) => {
   messages.push(event.data);
 };
 
-const router = new Router();
-router
-  .get("/", (context) => {
-    context.response.body = "Merhaba Dünya!";
-  })
-  .get("/messages", (context) => {
-    context.response.body = messages;
-  })
-  .post("/messages", async (context) => {
-    const message = await context.request.body().value;
-    messages.push(message);
-    channel.postMessage(message);
-    context.response.body = messages;
-  });
 
 const app = new Application();
 app.use(oakCors());
-app.use(routes.routes());
-app.use(routes.allowedMethods());
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 addEventListener("fetch", app.fetchEventHandler());
