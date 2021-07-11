756d1b28560ff26ef7d38c2d5e2416a0748cb671
Alperen <alperenakyuz@yandex.com>
2021-07-11 19:36:57 +0300

    Second Commit

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-addEventListener("fetch", (event) => {
-    event.respondWith(
-        new Response("Hello world", {
-            status: 200,
-            headers: {
-                "content-type": "text/plain"
-            },
-        }),
-    );
-});
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
+
+const messages = [];
+const channel = new BroadcastChannel("chat");
+channel.onmessage = (event) => {
+  messages.push(event.data);
+};
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Merhaba Dünya!";
+  })
+  .get("/messages", (context) => {
+    context.response.body = messages;
+  })
+  .post("/messages", async (context) => {
+    const message = await context.request.body().value;
+    messages.push(message);
+    channel.postMessage(message);
+    context.response.body = messages;
+  });
+
+const app = new Application();
+app.use(oakCors());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+addEventListener("fetch", app.fetchEventHandler());
